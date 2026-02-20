5c67009941d043e887dc885ec836f6d0bb64c0e4
Аминджон Ахмадов <ahmadovaminjon54@gmail.com>
2026-02-20 11:04:44 +0000

    нашидестай лоиҳаи нав кушода намешава ма ба ягон акаунти дигам лоиҳа бкш

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,16 @@
 'use client';
 
+/**
+ * ТАНЗИМОТИ FIREBASE (Config)
+ * 
+ * Барои фаъол кардани базаи маълумот ва чат:
+ * 1. Дар Firebase Console лоиҳа созед.
+ * 2. Web App илова кунед.
+ * 3. Танзимоти (Config)-ро аз он ҷо гирифта ба ҷои ин код гузоред.
+ */
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyAs-Example-Key",
+  apiKey: "БА_ИН_ҶО_API_KEY_ГУЗОРЕД",
   authDomain: "hunar-yob.firebaseapp.com",
   projectId: "hunar-yob",
   storageBucket: "hunar-yob.appspot.com",
